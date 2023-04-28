26dfa59cea9266aa8599ac9b85a2010125a02e56
sumit <sumit@gmail.com>
2023-04-28 16:28:07 +0530

    hiii

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//importing node framework
-var express = require(‘express’);
-var app = express();//Respond with "hello world tu chutya hai2" for requests that hit our root "/"
-app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
-});//listen to port 80 by default
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+res.send('hello world');
+});
 app.listen(process.env.PORT || 80);
 module.exports = app;
